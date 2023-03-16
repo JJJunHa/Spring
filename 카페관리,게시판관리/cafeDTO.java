93e377c1a4194ca72ddc66c72a97a734069f2eb4
JJJunHa <121237261+JJJunHa@users.noreply.github.com>
2023-03-16 14:16:59 +0900

    Add files via upload

---
+++ 카페관리,게시판관리/cafeDTO.java
@@ -0,0 +1,14 @@
+package com.human.springboot;
+
+import lombok.Data;
+
+@Data
+public class cafeDTO {
+String created;
+String mobile;
+String menu_name;
+int gty;
+int price;
+	
+	
+}
---
+++ 카페관리,게시판관리/menuDTO.java
@@ -0,0 +1,11 @@
+package com.human.springboot;
+
+import lombok.Data;
+
+@Data
+public class menuDTO {
+  
+	private String menu;
+	private int price;
+	private int s_no;
+}
